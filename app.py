21fc36be5a6cec825b033c3bedbbce943103d9a5
PablooogiN Laptop <pabloginebra00@gmail.com>
2020-12-22 12:56:51 -0600

    added api key

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
+    response = requests.get("https://sandbox.iexapis.com/stable/stock/twtr/quote/latestPrice?token=Tsk_9980ed4d116a440cb98678f078a7c7c1")
+    # return render_template('index.html', response=response)
+    return response.content
 
 if __name__ == '__main__':
     app.run()
