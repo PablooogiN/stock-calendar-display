fd17683e4a018337efccd8c7aecf2ef290b58e5a
PablooogiN <ginebrapa@tamu.edu>
2021-01-14 12:03:00 -0600

    UI fixes and keys

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 import requests
+import os
 
 app = Flask(__name__)
 
@@ -7,19 +8,18 @@ app = Flask(__name__)
 def hello_world():
 
     # Write the stock ticker symbol for the 12 stocks you want to track
-    # stockList = ["spy", "qqq"]
     stockList = ["spy", "qqq","abnb","crsr","mu","rcl","ccl","nclh","ual","ba","nvda","abcl"]
+    IEX_DEBUG_API_KEY = os.environ.get("IEX_DEBUG_API_KEY")
+    API_KEYS = {'IEX_DEBUG_API_KEY': IEX_DEBUG_API_KEY}
 
     # response = requests.get("https://sandbox.iexapis.com/stable/stock/twtr/quote/latestPrice?token=Tsk_9980ed4d116a440cb98678f078a7c7c1")
     # response = requests.get("api.openweathermap.org/data/2.5/weather?q=houston&appid=d356074d2d6255803c9974a7ca75b175")
     # api.openweathermap.org/data/2.5/weather?q=London&appid=d356074d2d6255803c9974a7ca75b175
 
-    # https://sandbox.iexapis.com/stable/stock/twtr/delayed-quote?token=Tsk_9980ed4d116a440cb98678f078a7c7c1
-
     # latest price -> price
     # change percent -> day percent change
 
-    return render_template('index.html', stockList=stockList)
+    return render_template('index.html', stockList=stockList, API_KEYS=API_KEYS)
 
 if __name__ == '__main__':
     app.run()
--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,6 @@
+const timer = ms => new Promise(res => setTimeout(res, ms))
+var MARKET_OPEN = false;
+
 function load_data(){
         display_timeanddate();
         display_allstocks();
@@ -9,37 +12,51 @@ function display_c(){
 }
 
 function display_timeanddate() {
-        var x = new Date()
-        
         var myDate = new Date(); 
-        var myDay = myDate.getDay(); 
+        var myDay = myDate.getDay();
+        var hours = myDate.getHours();
+        var minutes = myDate.getMinutes();
         
         // Array of days. 
         var weekday = ['Sunday', 'Monday', 'Tuesday', 
             'Wednesday', 'Thursday', 'Friday', 'Saturday' 
         ];
         var currWeekday = weekday[myDay]
+
+        //Check if market is open
+        if(myDay > 0 & myDay < 6){
+                //time zone bug
+                if(hours >= 8 & hours <= 15){
+                        MARKET_OPEN=true;
+                }
+                else{
+                        MARKET_OPEN=false;
+                }
+        }
+        else{
+                MARKET_OPEN=false;
+        }
         
-        // get hour value. 
-        var hours = myDate.getHours(); 
+        // get hour value.
         var ampm = hours >= 12 ? 'PM' : 'AM'; 
         hours = hours % 12; 
         hours = hours ? hours : 12; 
-        var minutes = myDate.getMinutes(); 
+         
         minutes = minutes < 10 ? '0' + minutes : minutes; 
         var myTime = hours + ":" + minutes + " " + ampm;
 
         document.getElementById('time').innerHTML = myTime;
-        document.getElementById('day').innerHTML = currWeekday; 
+        document.getElementById('day').innerHTML = currWeekday;
         display_c();
 }
 
 function display_s(){
         var refresh=5*60000;
-        mytime=setTimeout('display_allstocks()',refresh);
+        if(MARKET_OPEN=true){
+                console.log("Market is open")
+                mytime=setTimeout('display_allstocks()',refresh);
+        }
 }
-
-const timer = ms => new Promise(res => setTimeout(res, ms))
 
 var forEach = async function (array, callback, scope) {
         for (var i = 0; i < array.length; i++) {
@@ -60,8 +77,8 @@ function display_allstocks(){
                                 console.log(data)
                                 // console.log(value.childNodes[1].innerHTML)
                                 latestPrice = data["latestPrice"];
-                                changePercent = data["changePercent"];
-                                value.childNodes[3].innerHTML = latestPrice;
+                                changePercent = Math.round((data["changePercent"] + Number.EPSILON) * 100) / 100;
+                                value.childNodes[3].innerHTML = "$"+latestPrice;
                                 value.childNodes[5].innerHTML = changePercent + "%";
                                 if (changePercent > 0){
                                         value.className = "stock positivePercent";
@@ -71,6 +88,7 @@ function display_allstocks(){
                                 }
                         }
                 );
+                // console.log(IEX_DEBUG_API_KEY);
                 // console.log(index, value); // passes index + value back!
         });
 
